733c70cd67fb7d1f0ff71e2cb3b36f4b109b9614
Gustavo Bento <gustavo.silva@movile.com>
2015-08-07 09:53:35 -0300

    Add event trigger to initial scene.

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour {
+	Color originalColor;
+
+	void Start(){
+		originalColor = gameObject.GetComponent<Image>().color;
+	}
+
+	public void changeColor(string color){
+		Debug.Log("ChangeColor");
+		if(color.Equals("white")){
+			gameObject.GetComponent<Image>().color = Color.white;
+			Debug.Log("White");
+		}
+		else if(color.Equals("green")){
+			gameObject.GetComponent<Image>().color = Color.green;
+			Debug.Log("Green");
+		}
+		else if(color.Equals("original")){
+			gameObject.GetComponent<Image>().color = originalColor;
+			Debug.Log("Original");
+		}
+	}
+}
---
+++ Assets/Scripts/Share.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Share : MonoBehaviour {
+
+	public void OnClick(){ 
+		#if UNITY_ANDROID 
+		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentAct = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+		currentAct.Call ("shareText", "Palmeiras", "Campeao");
+		#endif
+	}
+}
